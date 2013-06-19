4b557f398160ee8f3dc165faf9da5ee2ecc4480b
Ray Plante <rplante@ncsa.illinois.edu>
2013-06-19 14:16:39 +0000

    renamed conesearch.py to scs.py

--- python/vaopy/dal/__init__.py
+++ python/vaopy/dal/__init__.py
@@ -1,19 +1,18 @@
 from .sia import search as imagesearch
 from .ssa import search as spectrumsearch
 from .sla import search as linesearch
-from .    import conesearch as scs
-from .conesearch import search as conesearch
-# from .scs import search as conesearch
+from .scs import search as conesearch
 
 from .sia import SIAService, SIAQuery, SIARecord
 from .ssa import SSAService, SSAQuery, SSARecord
 from .sla import SLAService, SLAQuery, SLARecord
-from .conesearch import SCSService, SCSQuery, SCSRecord
-# from .scs import SCSService, SCSQuery, SCSRecord
+from .scs import SCSService, SCSQuery, SCSRecord
 
 from .query import *
 
 __all__ = [ "imagesearch", "spectrumsearch", "linesearch", "consesearch",
-            "SIARecord", "SSARecord", "SLARecord", "SCSRecord",
+            "SIAService", "SSAService", "SLAService", "SCSService",
+            "SIAResults", "SSAResults", "SLAResults", "SCSResults",
+            "SIARecord",  "SSARecord",  "SLARecord",  "SCSRecord",
             "DalResults" ]
 
