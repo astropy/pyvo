ce7cacc1a9221ca173c0d95b52f40d33b37c0259
Ray Plante <rplante@ncsa.uiuc.edu>
2013-06-22 07:34:35 -0500

    astropy infrastructure integration pt 3: added docs

---
+++ docs/pyvo/index.rst
@@ -0,0 +1,9 @@
+====
+PyVO
+====
+
+PyVO is a package providing access to remote data and services of the
+Virtual observatory (VO) using Python.
+
+
+
--- pyvo/__init__.py
+++ pyvo/__init__.py
@@ -19,6 +19,49 @@ access to these services.
 This module also exposes the exception classes raised by the above functions, 
 of which DALAccessError is the root parent exception. 
 """
+
+#this indicates whether or not we are in the pyvo's setup.py
+try:
+    _ASTROPY_SETUP_
+except NameError:
+    from sys import version_info
+    if version_info[0] >= 3:
+        import builtins
+    else:
+        import __builtin__ as builtins
+    builtins._ASTROPY_SETUP_ = False
+    del version_info
+
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = '0.0dev'
+try:
+    from .version import githash as __githash__
+except ImportError:
+    __githash__ = ''
+
+if not _ASTROPY_SETUP_:
+
+    import os
+    from warnings import warn
+    from astropy import config
+
+    # add these here so we only need to cleanup the namespace at the end
+    config_dir = None
+
+    if not os.environ.get('ASTROPY_SKIP_CONFIG_UPDATE', False):
+        config_dir = os.path.dirname(__file__)
+        try:
+            config.configuration.update_default_config(__package__, config_dir)
+        except config.configuration.ConfigurationDefaultMissingError as e:
+            wmsg = (e.args[0] + " Cannot install default profile. If you are "
+                    "importing from source, this is expected.")
+            warn(config.configuration.ConfigurationDefaultMissingWarning(wmsg))
+            del e
+
+    del os, warn, config_dir  # clean up namespace
+
 # make sure we have astropy
 import astropy.io.votable
 from . import registry
