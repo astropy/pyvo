3f1518758a4c146be782aa55ecd18065a6cb98f6
Ray Plante <rplante@ncsa.uiuc.edu>
2013-06-25 23:53:43 -0500

    fix version string format (thanks to keflavich)

--- pyvo/__init__.py
+++ pyvo/__init__.py
@@ -35,7 +35,7 @@ except NameError:
 try:
     from .version import version as __version__
 except ImportError:
-    __version__ = '0.0dev'
+    __version__ = '0.0.dev'
 try:
     from .version import githash as __githash__
 except ImportError:
