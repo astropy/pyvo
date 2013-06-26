c401b03a49d73e1d4558b4133a8869b4af694bcf
Ray Plante <rplante@ncsa.uiuc.edu>
2013-06-26 16:14:25 -0500

    completed license check

--- tests/testLicense.py
+++ tests/testLicense.py
@@ -9,20 +9,42 @@ license_ref_line = \
    "# Licensed under a 3-clause BSD style license - see LICENSE.rst"
 license_file = "licenses/LICENSE.rst"
 
-class LicenseTestCase(_testBase):
+class LicenseTestCase(unittest.TestCase):
 
     def assertHasLicenseRef(self, filename):
-        with open(path) as srcf:
-            lines = srcf.readLines()
+        with open(filename) as srcf:
+            lines = srcf.readlines()
 
         self.assertTrue(
-            len(filter(lamda ln: ln.startswith(license_ref_line), lines)) > 0,
+            len(filter(lambda ln: ln.startswith(license_ref_line), lines)) > 0,
             "%s does not have license reference line" % filename)
-        self.assertTrue(line[0].startswith(license_ref_line),
+        self.assertTrue(lines[0].startswith(license_ref_line),
                         "license reference line is not first line in %s" %
                         license_ref_line)
 
     def testHasLicense(self):
-        assertTrue(os.path.exists(license_file),
-                   "license/LICENSE.rst appears to be missing (what dir are you in?)")
+        self.assertTrue(os.path.exists(license_file),
+                        "license/LICENSE.rst appears to be missing (what dir are you in?)")
 
+def list_py_files(arg, dirname, names):
+    return map(lambda f: (f[:-3], os.path.join(dirname,f)), 
+               filter(lambda n: n.endswith(".py"), names))
+
+for dirp, dirs, files in os.walk("pyvo"):
+    for fname in files:
+        if not fname.endswith(".py"):
+            continue
+        path = os.path.join(dirp, fname)
+        name = re.sub(r'/', "_", path)
+        f = "lambda s: s.assertHasLicenseRef('%s')" % path
+        setattr(LicenseTestCase, "test_"+name, eval(f))
+
+__all__ = "LicenseTestCase".split()
+def suite():
+    tests = []
+    for t in __all__:
+        tests.append(unittest.makeSuite(globals()[t]))
+    return unittest.TestSuite(tests)
+
+if __name__ == "__main__":
+    unittest.main()
