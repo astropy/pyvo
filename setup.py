463fbdb9c18417f9084d6a678622ffed45e9297e
Ray Plante <rplante@ncsa.uiuc.edu>
2013-07-09 08:56:06 -0500

    prep early release: beta1

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ LICENSE = 'BSD'
 URL = 'http://dev.usvao.org/vao/wiki/Products/PyVO'
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.0.dev'
+VERSION = '0.0beta1'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
