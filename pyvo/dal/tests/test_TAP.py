64584f03567f2e5952c5f4c834c321dde14a695e
Stefan Becker <ich@funbaker.de>
2016-05-20 15:20:46 +0200

    more tests

--- pyvo/dal/tests/test_TAP.py
+++ pyvo/dal/tests/test_TAP.py
@@ -25,6 +25,24 @@ testserverport = 8084
 testserverport += 50
 testserverport += random.randint(0,9)
 
+class TAPServiceTest(unittest.TestCase):
+
+    baseurl = "http://localhost/sia"
+
+    def testCtor(self):
+        self.srv = tap.TAPService(self.baseurl)
+
+    def testProps(self):
+        self.testCtor()
+
+        self.assertEquals(self.srv.baseurl, self.baseurl)
+        self.assertEquals(self.srv.protocol, "tap")
+        try:
+            self.srv.baseurl = "towel"
+            self.fail("baseurl not read-only")
+        except AttributeError:
+            pass
+
 class TAPRunTest(unittest.TestCase):
     @classmethod
     def setup_class(cls):
@@ -44,9 +62,11 @@ class TAPRunTest(unittest.TestCase):
 
         s = tap.TAPService("http://localhost:{0}/tap".format(self.srvr.port))
 
-        q = s.run_sync(query)
+        r = s.run_sync(query)
 
-        self.assert_(isinstance(q, dalq.DALResults))
+        self.assert_(isinstance(r, tap.TAPResults))
+        self.assert_(r.query_status == "OVERFLOW")
+        self.assert_(len(r) == 1)
 
     def testRunAsync(self):
         query = "SELECT TOP 1 1+1 AS result FROM arihip.main"
@@ -58,11 +78,14 @@ class TAPRunTest(unittest.TestCase):
 
         q.run()
 
-        e = q.execute()
-        self.assert_(isinstance(e, dalq.DALResults))
+        r = q.execute()
+
+        self.assert_(isinstance(r, tap.TAPResults))
+        self.assert_(r.query_status == "OVERFLOW")
+        self.assert_(len(r) == 1)
 
 
-__all__ = "TAPRunTest".split()
+__all__ = "TAPServiceTest TAPRunTest".split()
 def suite():
     tests = []
     for t in __all__:
