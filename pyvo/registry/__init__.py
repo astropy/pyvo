d30a1908573b832ecf8a943fbf434c1950e85818
Stefan Becker <ich@funbaker.de>
2016-10-27 16:26:43 +0200

    add regtap interface similar to the existing vao interface and set it as standard

--- pyvo/registry/__init__.py
+++ pyvo/registry/__init__.py
@@ -5,5 +5,5 @@ a package for interacting with registries.
 The vao module supports the VAO-specific (non-standard) restful interfaces.
 Additional modules will be added in the future to access standard interfaces.
 """
-from .vao import *
-__all__ = [ "search", "RegistryService", "RegistryQuery" ]
+from .regtap import *
+__all__ = ["search"]
