f0ce34b28a881e39f7c8d099d70ce57714a8a2cc
Ray Plante <raymond.plante@nist.gov>
2016-11-30 20:27:31 -0500

    prep a (retroactive) tagging of v0.1

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ The pyvo module currently provides these main capabilities:
 builtins._ASTROPY_PACKAGE_NAME_ = PACKAGENAME
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.0.dev'
+VERSION = '0.1'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
