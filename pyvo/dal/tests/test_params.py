85bddbea93223479414a608950f4dc94a5fab8f6
Stefan Becker <ich@funbaker.de>
2018-08-15 11:02:54 +0200

    separate serialize tests from param tests

--- pyvo/dal/tests/test_params.py
+++ pyvo/dal/tests/test_params.py
@@ -51,8 +51,26 @@ def test_find_param_by_keyword():
     assert polygon_lower == polygon_upper
     assert circle_lower == circle_upper
 
-    polygon_conv = Converter.from_param(polygon_lower)
-    circle_conv = Converter.from_param(circle_lower)
+
+@pytest.mark.usefixtures('proc')
+@pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W06")
+@pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W48")
+@pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.E02")
+def test_serialize():
+    datalink = DatalinkResults.from_result_url('http://example.com/proc')
+    proc = datalink[0]
+    input_params = {param.name: param for param in proc.input_params}
+
+    polygon_conv = Converter.from_param(
+        find_param_by_keyword('polygon', input_params))
+    circle_conv = Converter.from_param(
+        find_param_by_keyword('circle', input_params))
+    scale_conv = Converter.from_param(
+        find_param_by_keyword('scale', input_params))
+    kind_conv = Converter.from_param(
+        find_param_by_keyword('kind', input_params))
 
     assert polygon_conv.serialize((1, 2, 3)) == "1.0 2.0 3.0"
     assert circle_conv.serialize((1, 2, 3)) == "1.0 2.0 3.0"
+    assert scale_conv.serialize(1) == "1"
+    assert kind_conv.serialize("DATA") == "DATA"
