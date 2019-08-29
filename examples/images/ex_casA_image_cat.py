48f8d0d9723a402b1e925fe3e305d2103fc2d544
Christine Banek <cbanek@lsst.org>
2019-08-29 12:50:13 -0700

    Update ex_casA_image_cat example
    Thanks @msdemlei for the updated working example.
    
    Taken verbatim from #170.
    
    Fixes #170.

--- examples/images/ex_casA_image_cat.py
+++ examples/images/ex_casA_image_cat.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python
-from astropy.coordinates import SkyCoord
 import pyvo as vo
+from astropy.coordinates import SkyCoord
+
+import warnings
+warnings.filterwarnings('ignore', module="astropy.io.votable.*")
 
 # find archives with x-ray images
-archives = vo.regsearch(servicetype='image', waveband='xray')
-
+archives = vo.regsearch(servicetype='image', waveband='x-ray')
 # position of my favorite source
 pos = SkyCoord.from_name('Cas A')
 
@@ -13,16 +15,16 @@ with open('cas-a.csv', 'w') as csv:
     print("Archive short name,Archive title,Image"
           "title,RA,Dec,format,URL", file=csv)
     for arch in archives:
-        print("searching %s..." % arch.shortname)
+        print("searching {}...".format(arch.short_name))
         try:
             matches = arch.search(pos=pos, size=0.25)
         except vo.DALAccessError as ex:
-            print("Trouble accessing %s archive (%s)"
-                  % (arch.shortname, str(ex)))
+            print("Trouble accessing {} archive ({})".format(
+                  arch.short_name, str(ex)))
             continue
-        print("...found %d images" % matches.nrecs)
+        print("...found {} images.".format(len(matches)))
         for image in matches:
             print(','.join(
-             (arch.shortname, arch.title, image.title,
-              str(image.ra), str(image.dec), image.format,
+             (arch.short_name, arch.res_title, image.title,
+              str(image.pos), image.format,
               image.getdataurl())), file=csv)
