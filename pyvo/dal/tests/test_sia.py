f35968563a698e53e6f81e89629c863a39ba8456
Adrian Damian <Adrian.Damian@nrc.ca>
2020-01-23 16:31:42 +1300

    Added support for SIAv2

--- pyvo/dal/tests/test_sia.py
+++ pyvo/dal/tests/test_sia.py
@@ -8,9 +8,11 @@ import re
 
 import pytest
 
-from pyvo.dal.sia import search, SIAService
+from pyvo.dal.sia import search, SIAService, search_v2
 
 from astropy.io.fits import HDUList
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 from astropy.utils.data import get_pkg_data_contents
 
 get_pkg_data_contents = partial(
@@ -63,3 +65,37 @@ class TestSIAService:
         result = results[0]
 
         _test_result(result)
+
+    @pytest.mark.usefixtures('sia')
+    @pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W06")
+    @pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W42")
+    @pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W49")
+    def test_search(self):
+        service = SIAService('http://example.com/sia')
+
+        positions = [
+            'CIRCLE 12.0 34.0 0.5',
+            'RANGE 12.0 12.5 34.0 36.0',
+            'POLYGON 12.0 34.0 14.0 35.0 14.0 36.0 12.0 35.0',
+            (SkyCoord('08h45m07.5s +54d18m00s'),
+             0.0166 * u.degree),
+            (12.0*u.degree, 12.5*u.degree, 34.0*u.degree, 36.0*u.degree),
+            (SkyCoord(12.0*u.degree, 34.0*u.degree),
+             SkyCoord(14.0*u.degree, 35.0*u.degree),
+             SkyCoord(14.0*u.degree, 36.0*u.degree),
+             SkyCoord(12.0*u.degree, 35.0*u.degree))]
+
+        for pos in positions:
+            results = service.search_v2(pos=pos)
+            result = results[0]
+            _test_result(result)
+
+
+@pytest.mark.usefixtures('sia')
+@pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W06")
+def test_search_v2():
+    results = search_v2('http://example.com/sia', pos='CIRCLE 12.0 34.0 0.5')
+    result = results[0]
+
+    _test_result(result)
+
