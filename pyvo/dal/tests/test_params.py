4dc191f6d44cb99ef6e37cbab818981e8d154490
Adrian Damian <Adrian.Damian@nrc.ca>
2020-04-22 10:09:04 +1200

    Fixed style

--- pyvo/dal/tests/test_params.py
+++ pyvo/dal/tests/test_params.py
@@ -8,7 +8,7 @@ from urllib.parse import parse_qsl
 
 from pyvo.dal.adhoc import DatalinkResults
 from pyvo.dal.params import find_param_by_keyword, get_converter,\
-AbstractDalQueryParam
+    AbstractDalQueryParam
 from pyvo.dal.exceptions import DALServiceError
 
 import pytest
