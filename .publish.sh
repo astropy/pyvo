62673af47ff1952f75eca1247e2ba144c9485e42
Adrian Damian <Adrian.Damian@nrc.ca>
2020-06-17 20:45:01 +1200

    Publish task

--- .publish.sh
+++ .publish.sh
@@ -3,24 +3,25 @@
 
 echo "Deploying version $TRAVIS_TAG..."
 # check that the version in the tag and in setup.cfg match
-sed 's/ //g' setup.cfg | grep "^version=$version" || { \
+sed 's/ //g' setup.cfg | grep "^version=$TRAVIS_TAG" || { \
    echo "Version in tag ($TRAVIS_TAG) does not match version in setup.cfg \
 ($(sed 's/ //g' setup.cfg | grep '^version=' | awk -F '=' '{print $2}'))"; \
-   exit -1; }
+   exit 255; }
 
 # build
-python setup.py clean sdist || { echo "Errors building"; exit -1; }
-# upload to pypi
-# generate the .pypirc file first
+python setup.py clean sdist || { echo "Errors building"; exit 255; }
+ upload to pypi
+ generate the .pypirc file first
+echo "[testpypi]" > .pypirc
 #echo "[pypi]" > .pypirc
-#chmod 600 .pypirc
-#echo "username = Canadian.Astronomy.Data.Centre" >> .pypirc
-#echo "password = ${PYPI_PASSWORD}" >> .pypirc
+chmod 600 .pypirc
+echo "username = adriand" >> .pypirc
+echo "password = ${PYPI_PASSWORD}" >> .pypirc
 echo "password = ${PYPI_PASSWORD}"
 
 echo "Publish on pypi ${TRAVIS_TAG}"
-#twine upload --config-file .pypirc dist/* || { echo "Errors publishing $TRAVIS_TAG"; exit -1; }
+twine upload --config-file .pypirc dist/* || { echo "Errors publishing $TRAVIS_TAG"; exit 255; }
 
 # check version available
-#pip uninstall -y $product
-#pip install --upgrade --pre $product==$version || { echo "$TRAVIS_TAG not installed on pypi" ; exit -1; } 
+pip uninstall -y pyvo
+pip install --upgrade --pre pyvo==$TRAVIS_TAG || { echo "$TRAVIS_TAG not installed on pypi" ; exit 255; }
