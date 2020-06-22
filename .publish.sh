13a720e2b47349009017fd193d0a3d65d248ec95
Adrian Damian <Adrian.Damian@nrc.ca>
2020-06-23 10:20:26 +1200

    Missed some changes first time around

--- .publish.sh
+++ .publish.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-# Deploys a Python library or application
+# Deploys a Python library or application to the PyPI repo
+# $TRAVIS_TAG is the version of the software and need to match the version
+# specified in the setup.cfg file. Calling environment must also provide the
+# value of the TWINE_PASSWORD environment variable.
 
 echo "Deploying version $TRAVIS_TAG..."
 # check that the version in the tag and in setup.cfg match
@@ -12,9 +15,9 @@ sed 's/ //g' setup.cfg | grep "^version=$TRAVIS_TAG" || { \
 python setup.py clean sdist bdist_wheel || { echo "Errors building"; exit 255; }
 #upload to pypi
 
-echo "Publishing pyvo ${TRAVIS_TAG} on pypi"
-export TWINE_USERNAME=adriand
-export TWINE_REPOSITORY_URL=https://test.pypi.org/legacy/  #TODO comment out unless testing
+echo "Publishing pyvo $TRAVIS_TAG on pypi"
+export TWINE_USERNAME=PYPIUSER  # TODO replace with real user
+export TWINE_REPOSITORY_URL=https://test.pypi.org/legacy/  # TODO comment out unless testing
 twine upload --verbose dist/* || { echo "Errors publishing $TRAVIS_TAG"; exit 255; }
 
 
