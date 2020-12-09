062710b30f21d330933856ae0d9b9592e9f1de8d
andamian <adrian.damian@nrc.ca>
2020-12-09 07:44:05 -0800

    Refine GitHub Actions (#246)
    Improved CI through GitHub Actions 
    
    Co-authored-by: P. L. Lim <2090236+pllim@users.noreply.github.com>

---
+++ conftest.py
@@ -0,0 +1,34 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# This file is the main file used when running tests with pytest directly,
+# in particular if running e.g. ``pytest docs/``.
+
+import os
+import tempfile
+
+try:
+    from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
+except ImportError:
+    PYTEST_HEADER_MODULES = {}
+    TESTED_VERSIONS = {}
+
+try:
+    from pyvo import __version__ as version
+except ImportError:
+    version = 'unknown'
+
+TESTED_VERSIONS['pyvo'] = version
+
+# Make sure we use temporary directories for the config and cache
+# so that the tests are insensitive to local configuration.
+
+os.environ['XDG_CONFIG_HOME'] = tempfile.mkdtemp('astropy_config')
+os.environ['XDG_CACHE_HOME'] = tempfile.mkdtemp('astropy_cache')
+
+os.mkdir(os.path.join(os.environ['XDG_CONFIG_HOME'], 'astropy'))
+os.mkdir(os.path.join(os.environ['XDG_CACHE_HOME'], 'astropy'))
+
+# Note that we don't need to change the environment variables back or remove
+# them after testing, because they are only changed for the duration of the
+# Python process, and this configuration only matters if running pytest
+# directly, not from e.g. an IPython session.
