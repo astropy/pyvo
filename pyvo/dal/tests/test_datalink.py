519ea280fc597ed78347d3af2c8c736da1605e3c
msdemlei <m@tfiu.de>
2021-04-08 22:53:09 +0200

    Make datalink .bysemantics semantics-aware (#241)
    * datalink .bysemantics turbocharge.
    
    For one, you can now pass in multiple terms in a list, with or without
    the leading hash.
    
    Then, the function by default expands to narrower terms, so that looking for
    (IMHO in almost all cases desirable) behaviour can be disabled by passing
    in include_narrower=False.

--- pyvo/dal/tests/test_datalink.py
+++ pyvo/dal/tests/test_datalink.py
@@ -8,6 +8,7 @@ from functools import partial
 import pytest
 
 import pyvo as vo
+from pyvo.dal.adhoc import DatalinkResults
 
 from astropy.utils.data import get_pkg_data_contents
 
@@ -67,6 +68,17 @@ def res_datalink(mocker):
         yield matcher
 
 
+@pytest.fixture()
+def proc(mocker):
+    def callback(request, context):
+        return get_pkg_data_contents('data/datalink/proc.xml')
+
+    with mocker.register_uri(
+        'GET', 'http://example.com/proc', content=callback
+    ) as matcher:
+        yield matcher
+
+
 @pytest.mark.usefixtures('ssa_datalink', 'datalink')
 @pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W27")
 @pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W06")
@@ -101,3 +113,52 @@ def test_datalink_batch():
         'http://example.com/obscore', (30, 30))
 
     assert len([_ for _ in results.iter_datalinks()]) == 3
+
+
+# temporary hack while astropy <4.1 is still around; you can remove
+# this and its calls once that's no longer true.
+def _debytify(v):
+    if isinstance(v, bytes):
+        return v.decode("utf-8")
+    return v
+
+
+@pytest.mark.usefixtures('proc')
+@pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W27")
+@pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W06")
+@pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.W48")
+@pytest.mark.filterwarnings("ignore::astropy.io.votable.exceptions.E02")
+class TestSemanticsRetrieval:
+    def test_access_with_string(self):
+        datalink = DatalinkResults.from_result_url('http://example.com/proc')
+        res = [_debytify(r["access_url"])
+            for r in datalink.bysemantics("#this")]
+        assert len(res)==1
+        assert res[0].endswith("eq010000ms/20100927.comb_avg.0001.fits.fz")
+
+    def test_access_with_list(self):
+        datalink = DatalinkResults.from_result_url('http://example.com/proc')
+        res = [_debytify(r["access_url"])
+            for r in datalink.bysemantics(["#this", "#preview-image"])]
+        assert len(res)==2
+        assert res[0].endswith("eq010000ms/20100927.comb_avg.0001.fits.fz")
+        assert res[1].endswith("20100927.comb_avg.0001.fits.fz?preview=True")
+
+    def test_access_with_expansion(self):
+        datalink = DatalinkResults.from_result_url('http://example.com/proc')
+        res = [_debytify(r["access_url"])
+            for r in datalink.bysemantics(["#this", "#preview"])]
+        assert len(res)==3
+        assert res[0].endswith("eq010000ms/20100927.comb_avg.0001.fits.fz")
+        assert res[1].endswith("20100927.comb_avg.0001.fits.fz?preview=True")
+        assert res[2].endswith("http://dc.zah.uni-heidelberg.de/wider.dat")
+
+    def test_access_without_expansion(self):
+        datalink = DatalinkResults.from_result_url('http://example.com/proc')
+        res = [_debytify(r["access_url"])
+            for r in datalink.bysemantics(
+               ["#this", "#preview"],
+               include_narrower=False)]
+        assert len(res)==2
+        assert res[0].endswith("eq010000ms/20100927.comb_avg.0001.fits.fz")
+        assert res[1].endswith("http://dc.zah.uni-heidelberg.de/wider.dat")
---
+++ pyvo/utils/vocabularies.py
@@ -0,0 +1,74 @@
+"""
+A shallow interface to IVOA vocabularies.
+
+See http://ivoa.net/documents/Vocabularies/ (>= version 2) for the
+larger background.  In this module, we essentially wrap the retrieval
+and caching of the desise files.
+"""
+
+import functools
+import json
+import os
+import time
+from urllib import request
+
+from astropy.utils.data import download_file, clear_download_cache
+
+IVOA_VOCABULARY_ROOT = "http://www.ivoa.net/rdf/"
+
+
+class VocabularyError(Exception):
+    """A generic error that occurred when interacting with the IVOA
+    vocabulary repository.
+    """
+
+
+@functools.lru_cache()
+def get_vocabulary(voc_name, force_update=False):
+    """returns an IVOA vocabulary in its "desise" form.
+
+    See Vocabularies in the VO 2 to see what is inside of this.
+
+    This will use a cache to avoid repeated updates, but it
+    will attempt to re-download if the cached copy is older than 6 months.
+    """
+    src_url = IVOA_VOCABULARY_ROOT+voc_name
+    if force_update:
+        clear_download_cache(src_url)
+
+    try:
+        src_name = download_file(
+            src_url,
+            cache=True,
+            show_progress=False,
+            http_headers={"accept": "application/x-desise+json"})
+    except Exception as msg:
+        raise VocabularyError("No such vocabulary: {} ({})".format(
+            voc_name, msg))
+
+    if time.time()-os.path.getmtime(src_name)>3600*60*150:
+        # attempt a re-retrieval, but ignore failure
+        try:
+            src_name = download_file(
+                IVOA_VOCABULARY_ROOT+voc_name,
+                cache="update", show_progress=False,
+                http_headers={"accept": "application/x-desise+json"})
+        except Exception as msg:
+            base.ui.notifyWarning("Updating cache for the vocabulary"
+                " {} failed: {}".format(voc_name, msg))
+
+    with open(src_name, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def get_label(voc, term, default=None):
+    """returns the label of term if it's in the desise vocabulary voc,
+    term capitalised otherwise.
+    """
+    if term in voc["terms"]:
+        return voc["terms"][term]["label"]
+    else:
+        return default
+
+
+# vi:et:sw=4:sta
