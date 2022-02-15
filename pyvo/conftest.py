dc4c248c46d24be0217ada4c5edad2ac21102501
Adrian Damian <Adrian.Damian@nrc.ca>
2022-02-15 07:04:54 -0800

    More rework after code review

--- pyvo/conftest.py
+++ pyvo/conftest.py
@@ -7,8 +7,6 @@ get picked up when running the tests inside an interpreter using
 
 """
 
-import os
-
 from astropy.version import version as astropy_version
 
 try:
@@ -36,18 +34,4 @@ def pytest_configure(config):
         PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
 
         from . import __version__
-        packagename = os.path.basename(os.path.dirname(__file__))
-        TESTED_VERSIONS[packagename] = __version__
-
-# Uncomment the last two lines in this block to treat all DeprecationWarnings as
-# exceptions. For Astropy v2.0 or later, there are 2 additional keywords,
-# as follow (although default should work for most cases).
-# To ignore some packages that produce deprecation warnings on import
-# (in addition to 'compiler', 'scipy', 'pygments', 'ipykernel', and
-# 'setuptools'), add:
-#     modules_to_ignore_on_import=['module_1', 'module_2']
-# To ignore some specific deprecation warning messages for Python version
-# MAJOR.MINOR or later, add:
-#     warnings_to_ignore_by_pyver={(MAJOR, MINOR): ['Message to ignore']}
-# from astropy.tests.helper import enable_deprecations_as_exceptions  # noqa
-# enable_deprecations_as_exceptions()
+        TESTED_VERSIONS['pyvo'] = __version__
