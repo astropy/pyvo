82bb4a44bd849909e283fa39d934cdc1b865f0e4
Dustin <at88mph@users.noreply.github.com>
2022-09-08 10:35:04 -0700

    ENH: Add operating system to user-agent (#344)
    * Add system to user-agent.

---
+++ pyvo/utils/tests/test_http.py
@@ -0,0 +1,14 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Tests for pyvo.utils.http
+"""
+
+import platform
+from pyvo.utils.http import create_session
+from pyvo.version import version
+
+
+def test_create_session():
+    system = platform.system()
+    test_session = create_session()
+    assert test_session.headers['User-Agent'] == 'python-pyvo/{} ({})'.format(version, platform.system())
