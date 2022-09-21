a22ee0a43cf388e7287847fdbf894c50606554f1
Brigitta Sipőcz <bsipocz@gmail.com>
2022-09-20 19:54:50 -0700

    MAINT: adding root level conftest to provide header for tox runs

--- conftest.py
+++ conftest.py
@@ -8,9 +8,9 @@ import tempfile
 
 try:
     from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
+    ASTROPY_HEADER = True
 except ImportError:
-    PYTEST_HEADER_MODULES = {}
-    TESTED_VERSIONS = {}
+    ASTROPY_HEADER = False
 
 # Make sure we use temporary directories for the config and cache
 # so that the tests are insensitive to local configuration.
@@ -21,20 +21,43 @@ os.environ['XDG_CACHE_HOME'] = tempfile.mkdtemp('astropy_cache')
 os.mkdir(os.path.join(os.environ['XDG_CONFIG_HOME'], 'astropy'))
 os.mkdir(os.path.join(os.environ['XDG_CACHE_HOME'], 'astropy'))
 
-try:
-    from pyvo import __version__ as version
-except ImportError:
-    version = 'unknown'
-
-TESTED_VERSIONS['pyvo'] = version
-
-
 # Note that we don't need to change the environment variables back or remove
 # them after testing, because they are only changed for the duration of the
 # Python process, and this configuration only matters if running pytest
 # directly, not from e.g. an IPython session.
 
+try:
+    from pyvo import __version__ as version
+except ImportError:
+    version = 'unknown'
+
+
 # Disable IERS auto download for testing (to support the local, non-remote-data scenario),
 # revisit this config when minimum supported astropy is 5.1.
 from astropy.utils.iers import conf as iers_conf
 iers_conf.auto_download = False
+
+
+def pytest_configure(config):
+    """Configure Pytest with Astropy.
+
+    Parameters
+    ----------
+    config : pytest configuration
+
+    """
+    if ASTROPY_HEADER:
+
+        config.option.astropy_header = True
+
+        # Customize the following lines to add/remove entries from the list of
+        # packages for which version numbers are displayed when running the tests.
+        PYTEST_HEADER_MODULES['Astropy'] = 'astropy'  # noqa
+        PYTEST_HEADER_MODULES['requests'] = 'requests'  # noqa
+
+        PYTEST_HEADER_MODULES.pop('Pandas', None)
+        PYTEST_HEADER_MODULES.pop('h5py', None)
+        PYTEST_HEADER_MODULES.pop('Scipy', None)
+        PYTEST_HEADER_MODULES.pop('Matplotlib', None)
+
+        TESTED_VERSIONS['pyvo'] = version
