d611f9d1eac73a3fc5ace919a017aafd77bc0a4c
Brigitta Sipőcz <bsipocz@gmail.com>
2023-01-27 08:58:10 -0800

    Merge pull request #410 from msdemlei/mention-more-servicetypes
    Improving servicetype documentation

--- pyvo/registry/__init__.py
+++ pyvo/registry/__init__.py
@@ -5,8 +5,8 @@ a package for interacting with registries.
 The regtap module supports access to the IVOA Registries
 """
 from .regtap import search, ivoid2service, get_RegTAP_query
-    
-from .rtcons import (Constraint, 
+
+from .rtcons import (Constraint,
     Freetext, Author, Servicetype, Waveband, Datamodel, Ivoid,
     UCD, Spatial, Spectral, Temporal)
 
