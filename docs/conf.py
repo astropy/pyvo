2b0e0fdf63c795f0dff9b714af3cab585f5507ed
Brigitta Sipőcz <bsipocz@gmail.com>
2023-01-26 17:43:37 -0800

    TST: enabling nitpicky mode to ensure sphinx links work

--- docs/conf.py
+++ docs/conf.py
@@ -161,3 +161,6 @@ if eval(setup_cfg.get('edit_on_github')):
 
     edit_on_github_source_root = ""
     edit_on_github_doc_root = "docs"
+
+
+nitpicky = True
--- docs/index.rst
+++ docs/index.rst
@@ -134,4 +134,5 @@ Using `pyvo`
    registry/index
    io/index
    auth/index
+   utils/index
    utils/prototypes
