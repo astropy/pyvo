16b490cf313a2d8ed979f2af75348f3becfc0f39
Brigitta Sipőcz <bsipocz@gmail.com>
2023-07-06 13:25:24 -0700

    Minor reformat of user-agent to address review

--- pyvo/utils/tests/test_http.py
+++ pyvo/utils/tests/test_http.py
@@ -12,4 +12,4 @@ from pyvo.version import version
 def test_create_session():
     test_session = create_session()
     assert (test_session.headers['User-Agent']
-            == f'python-pyvo/{version} ({platform.system()} {platform.python_version()})')
+            == f'pyVO/{version} Python/{platform.python_version()} ({platform.system()})')
