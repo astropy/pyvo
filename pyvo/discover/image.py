c4b6bdf75080770045d780aad2402e97e094ef73
Markus Demleitner <m@tfiu.de>
2023-08-31 11:57:32 +0200

    Fleshing out global image discovery.
    This moves towards using a few internal classes to represent found services
    and datasets to work around the inflexibility of the VOTables underlying
    DALResults.  Perhaps we should one day move back to the DALResults -- I
    think having some sort of view on them would actually be a smart thing.

--- pyvo/discover/image.py
+++ pyvo/discover/image.py
@@ -10,20 +10,18 @@ but that needs standard work.
 The code here looks for data in SIA1, SIA2, and Obscore services for now.
 """
 
-from enum import IntEnum
-from typing import List, Optional, Set, Tuple, TYPE_CHECKING
+import functools
 
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-from ..dal.sia2 import ObsCoreRecord
+from ..dam import obscore
 from .. import registry
 
 
 # imports for type hints
-from typing import List, Set
+from typing import List, Optional, Set, Tuple
 from astropy.units.quantity import Quantity
-from ..dal.sia import SIARecord
 
 
 class Queriable:
@@ -40,6 +38,16 @@ class Queriable:
         self.ivoid = res_rec.ivoid
 
 
+@functools.cache
+def obscore_column_names():
+    """returns the names of obscore columns.
+
+    For lack of alternatives, I pull them out of ObsCoreMetadata for now.
+    """
+    return [name for name in dir(obscore.ObsCoreMetadata())
+        if not name.startswith("_")]
+
+
 class ImageFound(obscore.ObsCoreMetadata):
     """Obscore metadata for a found record.
 
@@ -51,25 +59,47 @@ class ImageFound(obscore.ObsCoreMetadata):
     turning these into an astropy table and further into a VOTable
     to make this as compatible with the DAL services as possible.
     """
-    def __init__(self, **kwargs):
-        for k, v in kwargs.items()
+    attr_names = set(obscore_column_names())
+
+    def __init__(self, obscore_record):
+        for k, v in obscore_record.items():
+            if k not in self.attr_names:
+                raise TypeError(
+                    f"ImageFound does not accept {k} keys")
             setattr(self, k, v)
 
     @classmethod
     def from_obscore_recs(cls, obscore_result):
-        return TODO
-
-    @classmethod
-    def from_sia2_recs(cls, sia2_result):
-        return TODO
+        ap_table = obscore_result.to_table()
+        our_keys = [n for n in ap_table.colnames if n in cls.attr_names]
+        for row in obscore_result:
+            yield cls(dict(zip(our_keys, (row[n] for n in our_keys))))
 
     @classmethod
     def from_sia1_recs(cls, sia1_result, filter_func):
-        result = []
         for rec in sia1_result:
-            if filter_func(rec):
-                result.append(cls(**mapped))
-        return result
+            if not filter_func(rec):
+                continue
+
+            mapped = {
+                "dataproduct_type": "image" if rec.naxes == 2 else "cube",
+                "access_url": rec.acref,
+                "bandpass_hilimit": rec.em_max.to(u.m).value,
+                "bandpass_lolimit": rec.em_min.to(u.m).value,
+                # Sigh.  Try to guess exposure time?
+                "t_min": rec.dateobs,
+                "t_max": rec.dateobs,
+                "access_estsize": rec.filesize/1024,
+                "access_format": rec.format,
+                "instrument_name": rec.instr,
+                "s_xsel1": rec.naxis[0],
+                "s_xsel2": rec.naxis[1],
+                "s_ra": rec.pos[0],
+                "s_dec": rec.pos[1],
+                "obs_title": rec.title,
+                # TODO: do more (s_resgion!) on the basis of the WCS parts
+            }
+            yield cls(mapped)
 
 
 def _clean_for(records: List[Queriable], ivoids_to_remove: Set[str]):
@@ -100,7 +130,7 @@ class _ImageDiscoverer:
             self.radius = self.space[2]*u.deg
 
         self.inclusive = inclusive
-        self.result: List[ObsCoreRecord] = []
+        self.result: List[obscore.ObsCoreMetadata] = []
         self.log: List[str] = []
 
     def collect_services(self):
@@ -154,11 +184,18 @@ class _ImageDiscoverer:
         self.inclusive.
         """
         def non_spatial_filter(sia1_rec):
-            if not inclusive and self.spectral:
-                if not new_rec.em_min<self.spectral<new_rec.em_max:
+            if self.spectrum and not self.inclusive and (
+                    sia1_rec.bandpass_hilmit and sia1_rec.bandpass_lolimit):
+                if not (sia1_rec.bandpass_lolimit
+                        <= self.spectrum
+                        <= sia1_rec.bandpass_hilimit):
                     return False
-            if not inclusive and self.time:
-                if not new_rec.time_min<self.time<new_rec.time_max:
+
+            # Regrettably, the exposure time is not part of SIA1 standard
+            # metadata.  TODO: require time to be an interval and
+            # then replace check for dateobs to be within that interval.
+            if self.time and not self.inclusive and sia1_rec.dateobs:
+                if not self.time-1<sia1_rec.dateobs<self.time+1:
                     return False
             return True
 
@@ -168,7 +205,6 @@ class _ImageDiscoverer:
                 svc.search(
                     pos=self.center, size=self.radius, intersect='overlaps'),
                 non_spatial_filter))
-            self.results.append(new_rec)
 
     def _query_sia1(self):
         """runs the SIA1 part of our discovery.
@@ -185,14 +221,14 @@ class _ImageDiscoverer:
             try:
                 self._query_one_sia1(rec)
             except Exception as msg:
-                self.log.append(f"SIA1 {rec['ivoid']} skipped: {msg}")
+                self.log.append(f"SIA1 {rec.ivoid} skipped: {msg}")
 
     def _query_one_sia2(self, rec: Queriable):
         """runs our query against a SIA2 capability of rec.
         """
         svc = rec.res_rec.get_service("sia2")
         self.results.extend(
-            ImageFound.from_sia2_recs(
+            ImageFound.from_obscore_recs(
                 svc.search(pos=self.space, band=self.spectrum, time=self.time)))
 
     def _query_sia2(self):
@@ -202,7 +238,7 @@ class _ImageDiscoverer:
             try:
                 self._query_one_sia2(rec)
             except Exception as msg:
-                self.log.append(f"SIA2 {rec['ivoid']} skipped: {msg}")
+                self.log.append(f"SIA2 {rec.ivoid} skipped: {msg}")
 
     def _query_one_obscore(self, rec: Queriable, where_clause:str):
         """runs our query against a Obscore capability of rec.
@@ -226,7 +262,7 @@ class _ImageDiscoverer:
             meters = self.spectrum.to(u.m, equivalancies=u.spectral()).value
             where_parts.append(f"(em_min<={meters} AND em_max>={meters})")
         if self.time:
-            mjd = self.time.mjd
+            mjd = self.time.mjd.value
             where_parts.append(f"(t_min<={mjd} AND t_max>={mjd})")
 
         where_clause = "WHERE "+(" AND ".join(where_parts))
@@ -253,8 +289,8 @@ def discover_images(
         spectrum: Optional[Quantity]=None,
         time: Optional[float]=None,
         inclusive: bool=False
-        ) -> Tuple[List[ObsCoreRecord], List[str]]:
-    """returns a collection of ObsCoreRecord-s matching certain constraints
+        ) -> Tuple[List[obscore.ObsCoreMetadata], List[str]]:
+    """returns a collection of ObsCoreMetadata-s matching certain constraints
     and a list of log lines.
 
     Parameters
