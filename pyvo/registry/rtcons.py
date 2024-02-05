3feb2ad8499c20acd184000923f2ea89bd77ae3d
Markus Demleitner <m@tfiu.de>
2024-02-05 11:58:58 +0100

    Updating global discovery test data
    Also, some changes to make registry requests more predictable (i.e.,
    sorting the extra tables to have a deterministic order).

--- pyvo/registry/rtcons.py
+++ pyvo/registry/rtcons.py
@@ -956,7 +956,7 @@ def build_regtap_query(constraints, service):
 
     joined_tables = ["rr.resource", "rr.capability", "rr.interface",
                      "rr.alt_identifier"
-                     ] + list(extra_tables)
+                     ] + list(sorted(extra_tables))
 
     # see comment in regtap.RegistryResource for the following
     # oddity
