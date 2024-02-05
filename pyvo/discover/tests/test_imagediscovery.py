3eac8e4aae308912cc5c7fb7b83ef25c88cbe3b6
Markus Demleitner <m@tfiu.de>
2024-02-05 14:58:12 +0100

    Minor fix in SIA1 branch of global image discovery

--- pyvo/discover/tests/test_imagediscovery.py
+++ pyvo/discover/tests/test_imagediscovery.py
@@ -16,12 +16,6 @@ from pyvo.utils.testing import LearnableRequestMocker
 
 
 @pytest.fixture
-def _all_constraint_responses(requests_mock):
-    matcher = LearnableRequestMocker("image-with-all-constraints")
-    requests_mock.add_matcher(matcher)
-
-
-@pytest.fixture
 def _sia1_responses(requests_mock):
     matcher = LearnableRequestMocker("sia1-responses")
     requests_mock.add_matcher(matcher)
@@ -49,20 +43,24 @@ def test_single_sia1(_sia1_responses):
     assert "dc.zah.uni-heidelberg.de/getproduct/bgds/data" in im.access_url
 
 
+@pytest.fixture
+def _all_constraint_responses(requests_mock):
+    matcher = LearnableRequestMocker("image-with-all-constraints")
+    requests_mock.add_matcher(matcher)
+
+
 def test_cone_and_spectral_point(_all_constraint_responses):
     images, logs = discover.images_globally(
         space=(134, 11, 0.1),
         spectrum=600*u.eV)
 
-    assert ("SIA2 service <ivo://org.gavo.dc/__system__/siap2/sitewide>: 8 recs"
+    # TODO: this needs to be the obscore service when we have fixed
+    # obscore discovery
+    assert ("SIA2 GAVO Data Center SIAP Version 2 Service: 8 records"
         in logs)
 
     assert len(images) == 8
     assert images[0].obs_collection == "RASS"
-
-    # expected failure: the rosat SIA1 record should be filtered out
-    # by its relationship to the sitewide SIA2
-    assert len(logs) == 1
 
 
 @pytest.fixture
