efc1b6f69d783eca5ea375cad9a99c842dfdf555
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2025-04-03 15:11:54 +0200

    expose API components

--- pyvo/mivot/writer/__init__.py
+++ pyvo/mivot/writer/__init__.py
@@ -1,4 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from .annotations import MivotAnnotations
 from .instance import MivotInstance
-
+from .instances_from_models import InstancesFromModels
+from .mango_object import MangoObject, Property
