cf72648f479a57abaa99392b40b89856eb5b0ef4
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2025-09-04 15:45:57 +0200

    The viewer builds MivotInstances for INSTANCEs children of GLOBALS

--- pyvo/mivot/tests/test_mivot_viewer.py
+++ pyvo/mivot/tests/test_mivot_viewer.py
@@ -46,6 +46,120 @@ dm_raw_instances = [
     },
 ]
 
+globals_photcal = {
+    "dmid": "CoordSystem_XMM_EB1_id",
+    "dmtype": "Phot:PhotCal",
+    "identifier": {
+        "dmtype": "ivoa:string",
+        "value": "XMM/EPIC/EB1",
+        "unit": None,
+        "ref": None,
+    },
+    "magnitudeSystem": {
+        "dmrole": "Phot:PhotCal.magnitudeSystem",
+        "dmtype": "Phot:MagnitudeSystem",
+        "type": {
+            "dmtype": "Phot:TypeOfMagSystem",
+            "value": "XMM",
+            "unit": None,
+            "ref": None,
+        },
+        "referenceSpectrum": {
+            "dmtype": "ivoa:anyURI",
+            "value": "https://xmm-tools.cosmos.esa.int/external"
+                     "/xmm_user_support/documentation/sas_usg/USG/SASUSG.html",
+            "unit": None,
+            "ref": None,
+        },
+    },
+    "photometryFilter": {
+        "dmid": "CoordSystem_XMM_FILTER_EB1_id",
+        "dmtype": "Phot:PhotometryFilter",
+        "dmrole": "Phot:PhotCal.photometryFilter",
+        "identifier": {
+            "dmtype": "ivoa:string",
+            "value": "XMM/EPIC/EB1",
+            "unit": None,
+            "ref": None,
+        },
+        "name": {
+            "dmtype": "ivoa:string",
+            "value": "XMM EPIC EB1",
+            "unit": None,
+            "ref": None,
+        },
+        "description": {
+            "dmtype": "ivoa:string",
+            "value": "Soft",
+            "unit": None,
+            "ref": None,
+        },
+        "bandName": {
+            "dmtype": "ivoa:string",
+            "value": "EB1",
+            "unit": None,
+            "ref": None,
+        },
+        "spectralLocation": {
+            "dmrole": "Phot:PhotometryFilter.spectralLocation",
+            "dmtype": "Phot:SpectralLocation",
+            "ucd": {
+                "dmtype": "Phot:UCD",
+                "value": "em.wl.effective",
+                "unit": None,
+                "ref": None,
+            },
+            "unitexpression": {
+                "dmtype": "ivoa:Unit",
+                "value": "keV",
+                "unit": None,
+                "ref": None,
+            },
+            "value": {"dmtype": "ivoa:real", "value": 0.35, "unit": None, "ref": None},
+        },
+        "bandwidth": {
+            "dmrole": "Phot:PhotometryFilter.bandwidth",
+            "dmtype": "Phot:Bandwidth",
+            "ucd": {
+                "dmtype": "Phot:UCD",
+                "value": "instr.bandwidth;stat.fwhm",
+                "unit": None,
+                "ref": None,
+            },
+            "unitexpression": {
+                "dmtype": "ivoa:Unit",
+                "value": "keV",
+                "unit": None,
+                "ref": None,
+            },
+            "extent": {"dmtype": "ivoa:real", "value": 0.3, "unit": None, "ref": None},
+            "start": {"dmtype": "ivoa:real", "value": 0.2, "unit": None, "ref": None},
+            "stop": {"dmtype": "ivoa:real", "value": 0.5, "unit": None, "ref": None},
+        },
+        "transmissionCurve": {
+            "dmrole": "Phot:PhotometryFilter.transmissionCurve",
+            "dmtype": "Phot:TransmissionCurve",
+            "access": {
+                "dmrole": "Phot:TransmissionCurve.access",
+                "dmtype": "Phot:Access",
+                "reference": {
+                    "dmtype": "ivoa:anyURI",
+                    "value": "https://xmm-tools.cosmos.esa.int/external/xmm_user_support"
+                             "/documentation/sas_usg/USG/SASUSG.html",
+                    "unit": None,
+                    "ref": None,
+                },
+                "format": {
+                    "dmtype": "ivoa:string",
+                    "value": "text/html",
+                    "unit": None,
+                    "ref": None,
+                },
+            },
+        },
+    },
+}
+
 
 @pytest.mark.skipif(not check_astropy_version(), reason="need astropy 6+")
 def test_get_first_instance_dmtype(path_to_first_instance):
@@ -150,6 +264,30 @@ def test_instance_mutiple_in_templates(path_to_multiple_instance):
     for dmi in m_viewer.dm_instances:
         row_values.append(dmi.value.value)
     assert row_values == pytest.approx([1.0, 2.1, 3.2], rel=1e-3)
+
+
+@pytest.mark.skipif(not check_astropy_version(), reason="need astropy 6+")
+def test_globals_instances(path_to_multiple_instance):
+    """
+    Test case for the GLOBALS instance access as MivotInstances
+    """
+    m_viewer = MivotViewer(votable_path=path_to_multiple_instance)
+    instance_dict = []
+    photcals = 0
+    photfilters = 0
+    # test the DM instances children of TEMPLATES before their values are set
+    for dmi in m_viewer.dm_globals_instances:
+        if dmi.dmtype == "Phot:PhotCal":
+            photcals += 1
+        elif dmi.dmtype == "Phot:PhotometryFilter":
+            photfilters += 1
+        else:
+            assert False, f"Unexpected dmtype {dmi.dmtype} in GLOBALS "
+        instance_dict.append(dmi.to_hk_dict())
+    assert photcals == 3
+    assert photfilters == 3
+    # just check the first one
+    assert instance_dict[0] == globals_photcal
 
 
 def test_check_version(path_to_viewer):
